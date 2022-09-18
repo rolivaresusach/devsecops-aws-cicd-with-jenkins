0cb8756b2f4ae177940243b1f284a72671c0324d
rolivaresusach <110432517+rolivaresusach@users.noreply.github.com>
2022-09-18 01:28:39 -0300

    Update stop_flask1.sh

--- scripts/stop_flask1.sh
+++ scripts/stop_flask1.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py --user
-python3 -m pip install Flask
-python /web/scripts/stop_flask.py
+echo 'test'
+cd /web/scripts/
+python3 -m pip install Requests
+python stop_flask.py
